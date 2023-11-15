86798cd288576d8f0319a61b2ab3c48c93468a7e
Yoran Desktop <schouppey@gmail.com>
2023-11-15 19:24:28 +0100

    Add font weight to heading and update Hero section layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='h-[100vh] w-[100vw] mb-8 flex flex-col items-center px-8 py-12'>
-        <h1>Yoran Schouppe</h1>
+    <section className='h-[100vh] w-[100vw] mb-8 flex flex-col items-center justify-center px-4 py-10'>
+        <div className="flex flex-row gap-2 text-4xl mb-3">
+            <h1>Hi!</h1>
+            <h1>My name is...</h1>
+        </div>
+        <div className="flex flex-col gap-2 items-center text-center">
+            <h1 className='text-5xl'>Yoran Schouppe</h1>
+            <h1>Front-end Web & Mobile Developer / Designer</h1>
+        </div>
     </section>
   )
 }
