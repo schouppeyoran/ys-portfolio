b947af01c60d8a4034cc699bf6a0108257a3bede
Yoran Desktop <schouppey@gmail.com>
2023-11-16 01:24:04 +0100

    Refactor Backdrop component to improve performance and readability

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,28 +1,28 @@
-"use client";
+'use client'
 
-import React, { useEffect, useRef, ReactNode } from 'react';
-import { startFluidSimulation } from './fluidSimulation';
+import React, { useEffect, useRef, ReactNode } from 'react'
+import { startFluidSimulation } from './fluidSimulation'
 
 interface BackdropProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const Backdrop: React.FC<BackdropProps> = ({ children }) => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
   useEffect(() => {
     if (canvasRef.current) {
-      startFluidSimulation(canvasRef.current);
+      startFluidSimulation(canvasRef.current)
     }
-  }, []);
+  }, [])
 
   return (
-    <main className='flex flex-col items-center min-h-[100vh] overflow-hidden'>
-      <canvas ref={canvasRef} className='z-0 absolute w-[100%] h-[100%]'/>
-      <div className="z-1 absolute w-[100%] h-[100%] bg-woodsmoke opacity-70"/>
+    <main className="flex flex-col items-center min-h-[100vh] overflow-hidden">
+      <canvas ref={canvasRef} className="z-0 absolute w-[100%] h-[100%]" />
+      <div className="z-1 absolute w-[100%] h-[100%] bg-woodsmoke opacity-90" />
       {children}
     </main>
-  );
+  )
 }
 
-export default Backdrop;
+export default Backdrop
