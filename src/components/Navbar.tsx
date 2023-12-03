5f511ea5035d20fdcd698f9083c4fbffd45e44ca
Yoran Desktop <schouppey@gmail.com>
2023-12-04 00:35:41 +0100

    Add navbar menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+'use client'
+
+import React, { useState, FC } from 'react'
 import { IoMenu, IoChatboxEllipsesSharp } from 'react-icons/io5'
 
-const Navbar = () => {
+interface NavbarProps {
+  items: Array<string>
+}
+
+const Navbar: FC<NavbarProps> = ({ items }) => {
+  const [menuVisible, setMenuVisible] = useState(false)
+
   return (
     <nav className="bg-woodsmoke w-screen p-4 flex flex-row items-center justify-center shadow shadow-lava-red/50 z-100 fixed navbarAnimation">
       <div className="max-w-[1920px] flex-1 flex flex-row items-center px-3">
@@ -20,12 +28,19 @@ const Navbar = () => {
             Contact
           </a>
           <a
-            href="#contact"
-            className="text-sm p-1 bg-pale-carmine hover:text-white hover:bg-lava-red transition rounded flex flex-row items-center gap-1"
+            onClick={() => setMenuVisible(!menuVisible)}
+            className="text-sm p-1 bg-pale-carmine hover:text-white hover:bg-lava-red transition rounded flex flex-row items-center gap-1 cursor-pointer"
           >
             <IoMenu size={21} />
             Menu
           </a>
+          {menuVisible && (
+            <div className="absolute top-[105%] py-3 px-6 bg-pale-carmine rounded gap-3 flex flex-col navbarMenu z-90">
+              <h2>Test 1 a</h2>
+              <h2>Test 2 aaa</h2>
+              <h2>Test 3 aaaaaa</h2>
+            </div>
+          )}
         </div>
       </div>
     </nav>
