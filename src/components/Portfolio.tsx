f3bcc70af80ea639b45f202f102b6693424c4216
Yoran Desktop <schouppey@gmail.com>
2024-01-12 00:20:19 +0100

    Add portfolio index changing buttons

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useState, useEffect, useRef } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
@@ -34,6 +35,51 @@ const portfolioItems = [
       {
         name: 'Figma',
         icon: '/assets/icons/figma.svg',
+      },
+    ],
+  },
+  {
+    title: 'drFloorPlan',
+    description:
+      'drFloorPlan is a product that consists of a web application where casino proprietors can create floor plans of their casinos, and some modules in other products that can use the data created by the drFloorPlan builder module for several intra-casino use cases. I designed & developed the web app with NEXT.JS, using React Redux for state management, and Tailwind CSS for styling. I also designed & developed the player locating module, which is used in a React Native staff app (which I also designed & developed in a team) to help streamline the workflow of casino staff. I used React Native for the staff app, and I used Expo to build it. Finally, I also developed the business insights module, which is used in a React Native mobile AND NEXT.JS web app to display heat maps and data visualizations of the casino floor plans. The tech stack for each of these projects is pretty much uniform. I also did some of the graphic design work for the drFloorPlan products.',
+    link: 'https://drfloorplan.com',
+    images: [],
+    techStack: [
+      {
+        name: 'HTML5',
+        icon: '/assets/icons/html5.svg',
+      },
+      {
+        name: 'CSS3',
+        icon: '/assets/icons/css3.svg',
+      },
+      {
+        name: 'React',
+        icon: '/assets/icons/react.svg',
+      },
+      {
+        name: 'React Native',
+        icon: '/assets/icons/react.svg',
+      },
+      {
+        name: 'Next.js',
+        icon: '/assets/icons/nextjs.svg',
+      },
+      {
+        name: 'Tailwind CSS',
+        icon: '/assets/icons/tailwindcss.svg',
+      },
+      {
+        name: 'Figma',
+        icon: '/assets/icons/figma.svg',
+      },
+      {
+        name: 'Blender',
+        icon: '/assets/icons/blender.svg',
+      },
+      {
+        name: 'Photoshop',
+        icon: '/assets/icons/photoshop.svg',
       },
     ],
   },
@@ -78,12 +124,12 @@ const Portfolio = () => {
   }, [])
 
   return (
-    <div className="max-w-[1920px] w-[100%] flex flex-col items-center justify-center">
+    <div
+      id="projects"
+      className="max-w-[1920px] w-[100%] flex flex-col items-center justify-center"
+    >
       <h1 className="w-[100%] py-2 bg-pale-carmine text-center text-4xl rounded-t-lg">
         Portfolio
-      </h1>
-      <h1 className="text-xl py-2 bg-ironstone w-[100%] text-center">
-        Gianni DM
       </h1>
       <Swiper
         style={{}}
@@ -108,13 +154,13 @@ const Portfolio = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="w-full h-[25vh] bg-woodsmoke/50 border-t-2 border-b-2 border-pale-carmine px-2 pt-2 pb-4 overflow-y-scroll">
+      <div className="w-full h-[20vh] bg-woodsmoke/50 border-t-2 border-b-2 border-pale-carmine px-2 pt-2 pb-4 overflow-y-scroll">
         {mobileInterfaceToggle ? (
-          <div className="flex">
+          <div className="flex flex-row flex-wrap w-full items-start justify-center">
             {portfolioItems[currentItem].techStack.map((tech, index) => (
               <div
                 key={index}
-                className="flex flex-col items-center justify-center mx-2 flex-wrap w-full"
+                className="flex flex-col items-center justify-center mx-2"
               >
                 <img src={tech.icon} alt={tech.name} className="w-12 h-12" />
                 <p className="text-center">{tech.name}</p>
@@ -151,6 +197,34 @@ const Portfolio = () => {
         >
           Tech Stack
         </h1>
+      </div>
+      <div className="flex flex-row items-center justify-between w-full px-4 py-2 bg-ironstone w-[100%] rounded-bl-xl rounded-br-xl">
+        <button
+          onClick={() =>
+            setCurrentItem(
+              currentItem === 0 ? portfolioItems.length - 1 : currentItem - 1,
+            )
+          }
+          className="p-2"
+        >
+          <FaChevronLeft size={30} />
+        </button>
+        <div className="flex flex-col items-center">
+          <h2>{portfolioItems[currentItem].title}</h2>
+          <p>
+            {currentItem + 1}/{portfolioItems.length}
+          </p>
+        </div>
+        <button
+          onClick={() =>
+            setCurrentItem(
+              currentItem === portfolioItems.length - 1 ? 0 : currentItem + 1,
+            )
+          }
+          className="p-2"
+        >
+          <FaChevronRight size={30} />
+        </button>
       </div>
     </div>
   )
