8c39682c81fbeb80be66aab3f9386c181c6500c7
Yoran Desktop <schouppey@gmail.com>
2024-01-22 21:47:46 +0100

    Fix portfolio padding to fit navbar

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -126,7 +126,7 @@ const Portfolio = () => {
   return (
     <div
       id="projects"
-      className="max-w-[1920px] w-[100%] flex flex-col items-center justify-center px-2"
+      className="max-w-[1920px] w-[100%] flex flex-col items-center justify-center px-2 pt-16"
     >
       <h1 className="w-[100%] py-2 bg-pale-carmine text-center text-4xl rounded-t-lg">
         Portfolio
