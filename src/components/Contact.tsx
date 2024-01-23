5f90f8e724b0b2a6fd16152ba117ec76228aa2ac
Yoran Desktop <schouppey@gmail.com>
2024-01-23 21:26:17 +0100

    Add contact responsiveness for desktops

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -50,72 +50,79 @@ const Contact = () => {
         </div>
         <IoChatboxEllipsesSharp size={42} className="text-ironstone" />
       </div>
-      <form
-        ref={formRef}
-        className="flex flex-col items-stretch justify-start gap-2 w-full"
-        onSubmit={sendEmail}
-      >
-        <input
-          type="text"
-          name="user_name"
-          placeholder="Your name"
-          className="p-2 rounded-lg bg-white/20"
-          required
-          minLength={2}
-          maxLength={50}
-          pattern="[a-zA-Z\s]*"
-        />
-        <input
-          type="email"
-          name="user_email"
-          placeholder="Your email"
-          className="p-2 rounded-lg bg-white/20"
-          required
-        />
-        <textarea
-          name="message"
-          rows={5}
-          placeholder="Your message"
-          className="p-2 rounded-lg bg-white/20"
-          required
-          maxLength={200}
-        />
-        <button
-          type="submit"
-          className="p-2 rounded-lg bg-pale-carmine hover:bg-lava-red transition"
-          disabled={isSubmitting}
+      <div className="w-full flex flex-col items-center 2xl:flex-row 2xl:gap-4 2xl:items-stretch">
+        <form
+          ref={formRef}
+          className="flex flex-col items-stretch justify-start gap-2 w-full 2xl:w-[60%]"
+          onSubmit={sendEmail}
         >
-          Send
-        </button>
-      </form>
-      <p className="text-white/50 p-1">{result}</p>
-      <h1 className="text-3xl text-white/50 py-4">Or</h1>
-      <div className="w-full flex flex-row items-center justify-between gap-4">
-        <a
-          href="mailto:schouppey@gmail.com"
-          className="flex-1 flex flex-col items-center ga-2 p-2 bg-white/10 rounded-lg hover:bg-lava-red transition"
-        >
-          <h1>E-mail</h1>
-          <img
-            src="/assets/icons/gmail.svg"
-            alt=""
-            className="w-20 h-20 object-contain"
+          <input
+            type="text"
+            name="user_name"
+            placeholder="Your name"
+            className="p-2 rounded-lg bg-white/20"
+            required
+            minLength={2}
+            maxLength={50}
+            pattern="[a-zA-Z\s]*"
           />
-          <p className="text-xs">schouppey@gmail.com</p>
-        </a>
-        <a
-          href="https://t.me/MrBlonde245"
-          className="flex-1 flex flex-col items-center ga-2 p-2 bg-white/10 rounded-lg hover:bg-lava-red transition"
-        >
-          <h1>Telegram</h1>
-          <img
-            src="/assets/icons/telegram.svg"
-            alt=""
-            className="w-20 h-20 object-contain"
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Your email"
+            className="p-2 rounded-lg bg-white/20"
+            required
           />
-          <p className="text-xs">@MrBlonde245</p>
-        </a>
+          <textarea
+            name="message"
+            rows={5}
+            placeholder="Your message"
+            className="p-2 rounded-lg bg-white/20"
+            required
+            maxLength={200}
+          />
+          <button
+            type="submit"
+            className="p-2 rounded-lg bg-pale-carmine hover:bg-lava-red transition"
+            disabled={isSubmitting}
+          >
+            <h1>Send</h1>
+          </button>
+          <p className="text-white/50 p-1 2xl:hidden">{result}</p>
+        </form>
+        <h1 className="text-3xl text-white/50 py-4 2xl:my-auto">Or</h1>
+        <div className="w-full flex flex-row items-center justify-between gap-4  2xl:flex-col 2xl:items-stretch 2xl:flex-1">
+          <a
+            href="mailto:schouppey@gmail.com"
+            className="flex-1 flex flex-col items-center gap-2 p-2 bg-white/10 rounded-lg hover:bg-lava-red transition 2xl:flex-row 2xl:gap-8 2xl:p-3"
+          >
+            <div className="flex flex-col items-center">
+              <h1 className="2xl:text-xl">E-mail</h1>
+              <img
+                src="/assets/icons/gmail.svg"
+                alt=""
+                className="w-20 h-20 object-contain"
+              />
+            </div>
+            <p className="text-xs 2xl:text-lg">schouppey@gmail.com</p>
+          </a>
+          <a
+            href="https://t.me/MrBlonde245"
+            className="flex-1 flex flex-col items-center gap-2 p-2 bg-white/10 rounded-lg hover:bg-lava-red transition 2xl:flex-row 2xl:gap-8 2xl:p-3"
+          >
+            <div className="flex flex-col items-center">
+              <h1 className="2xl:text-xl">Telegram</h1>
+              <img
+                src="/assets/icons/telegram.svg"
+                alt=""
+                className="w-20 h-20 object-contain"
+              />
+            </div>
+            <p className="text-xs 2xl:text-lg">@MrBlonde245</p>
+          </a>
+        </div>
       </div>
+      <p className="hidden text-white/50 p-1 2xl:block">{result}</p>
     </div>
   )
 }
